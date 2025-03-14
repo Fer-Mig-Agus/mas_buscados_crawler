f7b5eedc6cd5cde0da102ac02a580f80ca561af0
Gabriel-Dev27 <ibanezgabrielignacio@gmail.com>
2025-03-14 06:33:03 -0300

    Se creo el entorno virtual y la rama_dev

---
+++ crawler.py
@@ -0,0 +1,14 @@
+#Datos necesarios:
+
+#Genero (gender)
+#Apellido (last_name)
+#Segundo apellido (second_name)
+#Nombre (first_name)
+#Segundo nombre (middle_name)
+#DNI (id_numer)
+#Fecha de Nacimiento (date_of_birth)
+#Nacionalidad (nationality)
+#Edad (age)
+#Extraer la mayor informacion posible de cada individuo, en el caso de no contener un dato, directamente colocar N/A. El proyecto debera de contar con los siguientes archivos
+
+##Cada vez que se actualiza la pagina coloca de manera diferente a las personas(Los va colocando de manera aleatoria)
